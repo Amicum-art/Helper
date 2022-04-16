341a449d2f000f6137308bbbb5166c5882574f46
Lantris <kosart00@mail.ru>
2022-04-16 21:11:10 +0300

    even or odd

--- Helper.cpp
+++ Helper.cpp
@@ -1,13 +1,12 @@
 ﻿#include <iostream>
 #include "Helper.h"
-#include <string>
+
 
 
 int main()
 {
-	std::string result = std::to_string(pow_double(132.647));
-	std::cout << result << ' ' << result.length() << ' ' << result[0] 
-			<< ' ' << result[result.length()-1] << std::endl;
-	std::cout << pow_double(132.647);
+	FindOddNumbers(41, true);
+	std::cout << std::endl;
+	FindOddNumbers(53, false);
 	return 0;
 }
--- Helper.h
+++ Helper.h
@@ -5,3 +5,7 @@ double pow_double(const double& num)
 	return num * num;
 }
 
+void FindOddNumbers(int Limit, bool IsOdd) {
+	for (int num = IsOdd ? 1 : 0; num < Limit; num += 2)
+		std::cout << num << ' ';
+}
